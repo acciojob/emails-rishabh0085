2efbc64afb7dfa1149ca3f1c1e1a5fbe5876a718
rishabh14 <rishabhsingh29234@gmail.com>
2023-12-28 14:20:37 +0530

    Email01

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -8,25 +8,18 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class Workspace extends Gmail {
-
-    private ArrayList<Meeting> calendar; // Stores all the meetings
+    private ArrayList<Meeting> calendar;
 
     public Workspace(String emailId) {
-        // The inboxCapacity is equal to the maximum value an integer can store.
         super(emailId, Integer.MAX_VALUE); // Default inbox capacity is set to maximum integer value
         this.calendar = new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting) {
-        //add the meeting to calendar
         calendar.add(meeting);
     }
 
     public int findMaxMeetings() {
-        // find the maximum number of meetings you can attend
-        // 1. At a particular time, you can be present in at most one meeting
-        // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
-        // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
         Collections.sort(calendar, Comparator.comparing(Meeting::getEndTime));
 
         int maxMeetings = 0;
@@ -38,6 +31,7 @@ public class Workspace extends Gmail {
                 lastEndTime = meeting.getEndTime();
             }
         }
+
         return maxMeetings;
     }
 }
