83a46776a9ef12a4a790f1a876c743292556d53e
rishabh14 <rishabhsingh29234@gmail.com>
2023-12-28 14:15:00 +0530

    Email

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -18,12 +18,25 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword)
+    {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if (oldPassword.equals(password) && isValidPassword(newPassword)) {
+            this.password = newPassword;
+        }
     }
+        private boolean isValidPassword(String newPassword)
+        {
+            // Password conditions: at least 8 characters, one uppercase, one lowercase, one digit, one special character
+            return newPassword.length() >= 8 &&
+                    newPassword.matches(".*[A-Z].*") &&
+                    newPassword.matches(".*[a-z].*") &&
+                    newPassword.matches(".*\\d.*") &&
+                    newPassword.matches(".*[^A-Za-z\\d].*");
+        }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -5,26 +5,39 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
-public class Workspace extends Gmail{
+public class Workspace extends Gmail {
 
     private ArrayList<Meeting> calendar; // Stores all the meetings
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId, Integer.MAX_VALUE); // Default inbox capacity is set to maximum integer value
+        this.calendar = new ArrayList<>();
     }
 
-    public void addMeeting(Meeting meeting){
+    public void addMeeting(Meeting meeting) {
         //add the meeting to calendar
-
+        calendar.add(meeting);
     }
 
-    public int findMaxMeetings(){
+    public int findMaxMeetings() {
         // find the maximum number of meetings you can attend
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+        Collections.sort(calendar, Comparator.comparing(Meeting::getEndTime));
 
+        int maxMeetings = 0;
+        LocalTime lastEndTime = LocalTime.MIN;
+
+        for (Meeting meeting : calendar) {
+            if (meeting.getStartTime().compareTo(lastEndTime) >= 0) {
+                maxMeetings++;
+                lastEndTime = meeting.getEndTime();
+            }
+        }
+        return maxMeetings;
     }
 }
